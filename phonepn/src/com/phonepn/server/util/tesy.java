b0d3f6f62af8aa4058ea8c5b940e8c0ecfbed69b
chenkuifang <314287696@qq.com>
2017-01-21 11:59:12 +0800

    new

---
+++ phonepn/src/com/phonepn/server/util/tesy.java
@@ -0,0 +1,15 @@
+package com.phonepn.server.util;
+
+/**
+ *
+ * 类说明
+ * 
+ * @author CKF
+ * @version 7.1
+ * @data 2017年1月21日
+ */
+public class tesy {
+	public static void main(String... strings) {
+		System.out.println("xxxxx");
+	}
+}
